f51178445af9605840deb6844f0fa322bfffe4ce
Drew Balletto <db368@njit.edu>
2018-11-22 18:01:39 -0500

    Add ability to detect closed networks
    _NodeDict_ (New)
    - Input: Graph G
    - Output: A dictionary in which each key is a node and its value is a
    list of edges
    
    _beginExploration_ (New)
    - Input: Node n
    - Output: All nodes connected to N
    - Is used to set up the explore method

--- reader.py
+++ reader.py
@@ -1,10 +1,13 @@
 import operator
 import sys
 
-datapath = "datasets/soc-Epinions1.txt"
+datapath = "datasets/Wiki-Vote.txt"
+visited = set()
+layer = 0
+
 
 # Returns a list of tuples containing edges in the form [node1,node2]
-def pullNodes(f):
+def pullNodes(f): # For task 2
     
     # Start off with a quick check to see if the file exists
     try:
@@ -52,8 +55,7 @@ def pullNodes(f):
 # of influencers. A node's rank as an influencer is determined by its 
 # _In-Degree_ which is the  number of edges into it. Only relevant in a directed 
 # graph
-
-def findInfluencers(d, de):
+def findInfluencers(d, de): # For Task 3
     nodes ={}
 
     # If we're looking for the in degree, we care about what is being linked
@@ -81,6 +83,77 @@ def findInfluencers(d, de):
     # Return top 3% of influencers
     return sortednodes[::-1][0:topsize] 
 
+# Returns graph as a dictionary, where each key is a node, and its value is a
+# list of edges out of it
+def nodeDict(d):     
+    nodes ={}
+   
+    # Iterate through list of edges
+    for edge in d:
+        
+        # Check to see if if we've recorded this node yet, if not, create a new
+        # list
+        if not edge[0] in nodes:
+            nodes[edge[0]] = [edge[1]]
+        else:
+            nodes[edge[0]].append(edge[1])
+
+    return nodes
+
+# Takes in a Node n, then returns all connected nodes
+def beginExploration(n):
+    global visited # A set of all visited nodes
+    global layer   # Recursion Depth for Debug Purposes
+
+    visited = set() # Reset visited nodes
+    layer = 0 # Reset Recursion Depth
+
+    return explore(n)
+
+# Returns nodes connected to N
+def explore(v):
+
+    # Define Global Variables
+    global visited
+    global layer
+    global G
+
+    # Mark v as visited   
+    layer = layer + 1
+    visited.add(v)
+
+    # Quick check to see if this node exists in our dictionary
+    if v not in G or G[v] is None:
+        return
+    
+    # Iterate through all edges of v
+    for edge in G[v]:
+
+        # Check to see if we've already visited v , or if it even has
+        # any edges out of it 
+        if edge not in visited and edge in G:
+                
+                #It has edges, go one layer deeper
+                explore(edge)
+
+    layer = layer-1
+
+    # Return visited edges
+    return visited
+
+
+# Report nodes that are either disconnected, or only have some connections
+# to each other. 
+def cleanNetwork(d): # Task 4
+    # Depth first search can be used to check if a graph is connected
+    
+    # Two nodes u and v of a directed graph are connected if there is a path 
+    # from u to v and a path from v to u
+    
+    # The node that receives the highest post number in a depth-first search 
+    # must lie in a source strongly connected component
+    return None
+
 # Pull nodes from the specified datapath
 dset = pullNodes(datapath)
 
@@ -88,4 +161,10 @@ dset = pullNodes(datapath)
 if dset == None:
     sys.exit()
 
-print(findInfluencers(dset, "in"))
+# Generate a dictionary of Nodes for fast access, then generate a list of influencers
+G = nodeDict(dset)
+influencers = findInfluencers(dset, "in")
+
+# Explore all nodes connected to the biggest influencer
+network = beginExploration(influencers[0][0])
+print("VISITED NODES", network)
